fd730efdd6e68f9ab4dd556f17bd71a5c3d9abe1
lbeen <214652809@qq.com>
2018-11-02 22:26:26 +0800

    spark学习

--- src/main/scala/spark/WebLogMax.scala
+++ src/main/scala/spark/WebLogMax.scala
@@ -1,5 +1,6 @@
 package spark
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partitioner, SparkConf, SparkContext}
 
 object WebLogMax {
@@ -9,10 +10,23 @@ object WebLogMax {
     //读取符合要求的行
     val lines = sc.textFile("/spark_test/weblog2.txt").map(_.split("\t"))
 
-//    lines.map((_(0),_(1))).pa
+    val partitioned: RDD[(String, String)] = lines.map(line => (line(0), line(1))).partitionBy(new WebPatitioner)
+
+    val reduced: RDD[((String, String), Int)] = partitioned.map((_, 1)).reduceByKey(_ + _)
+
+    val grouped: RDD[(String, String, Int)] = reduced.groupBy(_._1._1).map(t => {
+      val head = t._2.toList.sortBy(_._2).reverse.head
+      (t._1, head._1._2, head._2)
+    })
+
+    val sorted = grouped.sortBy(_._3)
+
+    sorted.saveAsTextFile("/spark_test/res2")
   }
 }
 
-class WebPatitioner extends Partitioner{
+class WebPatitioner extends Partitioner {
+  override def numPartitions: Int = 2
 
+  override def getPartition(key: Any): Int = if (key.toString.startsWith("www.")) 0 else 1
 }
