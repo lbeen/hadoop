81cd83c1443c951a7b0859a43d979509f6f05849
lbeen <214652809@qq.com>
2018-11-10 23:29:23 +0800

    spark sql学习

--- src/main/scala/spark/sql/WebLog2.scala
+++ src/main/scala/spark/sql/WebLog2.scala
@@ -3,37 +3,44 @@ package spark.sql
 import java.net.URL
 
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{Row, SparkSession}
 
 /**
-  * scala版网站访问日志统计
+  * scala sql版网站访问日志统计
   */
 object WebLog2 {
   def main(args: Array[String]): Unit = {
     val builder = SparkSession.builder().appName("WebLog2Sql").master("local[*]")
 
+    //获取session
     val session = builder.getOrCreate()
 
-
-
+    //从session获取sparkcontext
     val sc = session.sparkContext
 
     //读取符合要求的行
     val lines = sc.textFile("/spark_test/weblog.txt").map(_.split("\t")).filter(_.length == 6)
 
+    //读取数据生成rowRDD
     val row = lines.map(line => Row(line(0), line(1), new URL(line(5)).getHost))
 
+    //表结构
     val structType = StructType {
       Array(StructField("TIME", StringType), StructField("FWID", StringType), StructField("HOST", StringType))
     }
 
+    //创建dataframe
     val dataframe = session.createDataFrame(row, structType)
 
+    //创建零时视图
     dataframe.createTempView("V_WEB")
 
-    val result = session.sql("SELECT HOST,COUNT(1) COUNTS FROM V_WEB GROUP BY HOST ORDER BY COUNTS DESC")
+    //sql
+    val result = session.sql("SELECT HOST,COUNT(1) COUNTS FROM V_WEB GROUP BY HOST ORDER BY COUNTS DESC LIMIT 50")
 
-    result.write.json("/spark_test/weblog.txt")
+    //写入json
+    result.write.json("/spark_test/res2")
+
     //停止spack任务
     session.close()
   }
