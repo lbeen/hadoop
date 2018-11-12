e028f4caa8aef9d5b2f9b844af44ddf25a1d76f5
lbeen <214652809@qq.com>
2018-11-12 20:32:49 +0800

    spark sql学习

--- src/main/scala/spark/sql/WebLog2DataSet.scala
+++ src/main/scala/spark/sql/WebLog2DataSet.scala
@@ -1,9 +1,6 @@
 package spark.sql
 
-import java.net.URL
-
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 /**
   * scala sql版网站访问日志统计
@@ -15,33 +12,9 @@ object WebLog2DataSet {
     //获取session
     val session = builder.getOrCreate()
 
-    //从session获取sparkcontext
-    val sc = session.sparkContext
+    import session.implicits._
+    val lines: Dataset[Array[String]] = session.read.textFile("/spark_test/weblog.txt").map(_.split("\t")).filter(_.length == 6)
 
-    //读取符合要求的行
-    val lines = sc.textFile("/spark_test/weblog.txt").map(_.split("\t")).filter(_.length == 6)
 
-    //读取数据生成rowRDD
-    val row = lines.map(line => Row(line(0), line(1), new URL(line(5)).getHost))
-
-    //表结构
-    val structType = StructType {
-      Array(StructField("TIME", StringType), StructField("FWID", StringType), StructField("HOST", StringType))
-    }
-
-    //创建dataframe
-    val dataframe = session.createDataFrame(row, structType)
-
-    //创建零时视图
-    dataframe.createTempView("V_WEB")
-
-    //sql
-    val result = session.sql("SELECT HOST,COUNT(1) COUNTS FROM V_WEB GROUP BY HOST ORDER BY COUNTS DESC LIMIT 50")
-
-    //写入json
-    result.write.json("/spark_test/res2")
-
-    //停止spack任务
-    session.close()
   }
 }
